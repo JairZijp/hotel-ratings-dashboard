a57c84859dec2523cad794c5e140ccc745a4ba75
JairZijp <jairzijp123@gmail.com>
2019-12-30 12:29:24 +0100

    preprocess

--- sentiment_analyses.py
+++ sentiment_analyses.py
@@ -22,9 +22,36 @@ collection_name = 'balanced_reviews'
 db_cm = mng_db[collection_name].find()
 
 # Expand the cursor and construct the DataFrame
-df = pd.DataFrame(list(db_cm))
-del df['_id']
+reviews = pd.DataFrame(list(db_cm))
+del reviews['_id']
 
 import seaborn as sns
 
-sns.countplot(x='positive', data=df)
+sns.countplot(x='positive', data=reviews)
+
+def preprocess_text(sen):
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    return sentence
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+X = []
+sentences = list(reviews['review'])
+for sen in sentences:
+    X.append(preprocess_text(sen))
+
+X[3]
