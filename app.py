de9788b65c1f60a0886f10527612d0c988c1bfbe
JairZijp <jairzijp123@gmail.com>
2020-01-11 22:27:10 +0100

    map

--- app.py
+++ app.py
@@ -1,31 +1,97 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import pymongo
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import *
+
+mng_client = pymongo.MongoClient('localhost', 27017)
+mng_db = mng_client['assignment2']
+collection_name = 'reviews'
+db_cm = mng_db[collection_name].find()
+
+df = pd.DataFrame(list(db_cm))
+df.drop_duplicates(subset ="Hotel_Address", inplace = True) 
+print(df)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+df['text'] = df['Hotel_Name'] + '<br>Rating ' + (df['Average_Score']/1e6).astype(str)+' '
+limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+colors = ["rgb(0,116,217)","rgb(255,65,54)","rgb(133,20,75)","rgb(255,133,27)","lightgrey"]
+cities = []
+scale = 5000
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+for index, row in df.iterrows():
+    city = go.Scattergeo(
+        lon = [row['lng']],
+        lat = [row['lat']],
+        text = row['Hotel_Name'],
+        marker = go.scattergeo.Marker(
+            size = row['Average_Score'],
+            line = go.scattergeo.marker.Line(
+                width=0.5, color='rgb(40,40,40)'
+            )
+        ),
+        name = row["Hotel_Name"] )
+    cities.append(city)
+layout = go.Layout(
+        title = go.layout.Title(
+            text = 'Hote reviews'
+        ),
+        showlegend = True,
+        geo = go.layout.Geo(
+            scope = 'world',
+            projection = go.layout.geo.Projection(
+                type='mercator'
+            ),
+            showland = True,
+            landcolor = 'rgb(217, 217, 217)',
+            subunitwidth=1,
+            countrywidth=1,
+            subunitcolor="rgb(255, 255, 255)",
+            countrycolor="rgb(255, 255, 255)"
+        )
+    )
 
+fig = go.Figure(data=cities, layout=layout)
+
+app.layout = html.Div([
+    #dcc.Graph(fig)
     dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
+         id='example-graph',
+         figure={
+             'data': cities,
+             'layout': layout
+         }
     )
 ])
+
+
+# app.layout = html.Div(children=[
+#     html.H1(children='Hello Dash'),
+
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+
+#     dcc.Graph(
+#         id='example-graph',
+#         figure={
+#             'data': [
+#                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+#                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+#             ],
+#             'layout': {
+#                 'title': 'Dash Data Visualization'
+#             }
+#         }
+#     )
+# ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- sentiment_analyses.py
+++ sentiment_analyses.py
@@ -27,8 +27,6 @@ del reviews['_id']
 
 import seaborn as sns
 
-sns.countplot(x='positive', data=reviews)
-
 def preprocess_text(sen):
     # Removing html tags
     sentence = remove_tags(sen)
@@ -54,4 +52,75 @@ sentences = list(reviews['review'])
 for sen in sentences:
     X.append(preprocess_text(sen))
 
-X[3]
+
+X_train, X_test, y_train, y_test = train_test_split(X, reviews["positive"], test_size=0.20, random_state=42)
+
+# Create word-to-index dictionary.
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(X_train)
+
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+# Adding 1 because of reserved 0 index
+vocab_size = len(tokenizer.word_index) + 1
+
+maxlen = 100
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+
+from numpy import array
+from numpy import asarray
+from numpy import zeros
+
+embeddings_dictionary = dict()
+glove_file = open('D:/glove.6B.100d.txt', encoding="utf8")
+
+for line in glove_file:
+    records = line.split()
+    word = records[0]
+    vector_dimensions = asarray(records[1:], dtype='float32')
+    embeddings_dictionary [word] = vector_dimensions
+glove_file.close()
+
+embedding_matrix = zeros((vocab_size, 100))
+for word, index in tokenizer.word_index.items():
+    embedding_vector = embeddings_dictionary.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[index] = embedding_vector
+
+
+model = Sequential()
+embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=maxlen , trainable=False)
+model.add(embedding_layer)
+
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+print(model.summary())
+
+#train model
+history = model.fit(X_train, y_train, batch_size=128, epochs=3, verbose=1, validation_split=0.2)
+
+score = model.evaluate(X_test, y_test, verbose=1)
+print("Test Score:", score[0])
+print("Test Accuracy:", score[1])
+
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train','test'], loc='upper left')
+# plt.show()
+
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train','test'], loc='upper left')
+# plt.show()
